f2681450f8a36b9b17c76f7fae99de0a736de2aa
TCKACHIKSIS <71429999+TCKACHIKSIS@users.noreply.github.com>
2021-11-13 15:42:22 +1000

    add datatable cell and struct for location and time

---
+++ fefuactivity/Cells/OneStatementInActivity/location_data_struct.swift
@@ -0,0 +1,18 @@
+//
+//  location_data_struct.swift
+//  fefuactivity
+//
+//  Created by иван on 13.11.2021.
+//
+
+import Foundation
+import UIKit
+
+struct LocationData{
+    let how_time_ago = String();
+    let type_of_activnosti = String();
+    let image_of_activnosti = UIImage();
+    let long_of_activnosi = String();
+    let long_time_of_activnosti = String();
+    let header_time_of_activnost = String();
+}
